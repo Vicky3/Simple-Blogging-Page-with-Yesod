f4aad039eada4979ceb20f65273880962417b69a
star <star@star-VirtualBox.(none)>
2015-08-05 22:56:14 +0200

    alles repariert, wird cool werden

--- Handler/AddPost.hs
+++ Handler/AddPost.hs
@@ -1,8 +1,8 @@
 module Handler.AddPost where
 
 import Import
-import Yesod.Form.Nic (YesodNic, nicHtmlField)
 
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
 instance YesodNic App
 
 getAddPostR :: Handler Html
@@ -10,17 +10,29 @@ getAddPostR = do
                 (bPostWidget, theEnctype) <- generateFormPost aForm
                 defaultLayout $ [whamlet|
                   <h1>Add a new post
-                  <form method=get action=@{BlogR}><button>Return</form>    <form method=get action=@{SettingsR}><button>Settings</form>
+                  <form method=get action=@{BlogR 1}><button>Return</button></form>    <form method=get action=@{SettingsR}><button>Settings</button></form>
                   <form method=post action=@{AddPostR} enctype=#{theEnctype}>
+                    <noscript>To get a nice editor, please enable JavaScript!
                     ^{bPostWidget}
                     <button>Submit!
+                  <hr>
                 |]
 
 postAddPostR :: Handler Html
-postAddPostR = error "Not yet implemented: postAddPostR"
+postAddPostR = do
+                 ((res,bPostWidget),theEnctype) <- runFormPost aForm
+                 case res of
+                   FormSuccess bPost -> do
+                     bPostId <- runDB $ insert bPost
+                     redirect $ BlogR 1
+                   _ -> defaultLayout $ [whamlet|
+                   <h1>Sorry, something went wrong!
+                   <form method=get action=@{AddPostR}><button>Try again</button></form>    <form method=get action=@{BlogR 1}><button>Return to main page</button></form>
+                   |]
+                 
 
 aForm :: Form BlogPost
 aForm = renderDivs $ BlogPost
-  <$> areq textField "Title" Nothing
-  <*> areq nicHtmlField "Text" Nothing
-  <*> lift (liftIO getCurrentTime)
+    <$> areq textField "Title: " Nothing
+    <*> areq nicHtmlField "Text: " Nothing
+    <*> lift (liftIO getCurrentTime)
--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -2,16 +2,22 @@ module Handler.Blog where
 
 import Import
 
-getBlogR :: site -> Handler Html
+getBlogR :: Int -> Handler Html
 getBlogR site = do
-                  posts <- runDB $ selectList [] [Desc BlogPostDate, LimitTo 3]
+                  let previousPage = site-1
+                  let nextPage = site+1
+                  let postsPerSite = 3
+                  let firstPost = postsPerSite*(site-1)+1
+                  posts <- runDB $ selectList [] [Desc BlogPostDate, LimitTo postsPerSite, OffsetBy (postsPerSite*(site-1))]
+                  let lastPost = firstPost+length(posts)-1
                   defaultLayout $ [whamlet|
                     <h1>Welcome to a FANTASTIC Blog
-                    <button>Login    <form method=get action=@{AddPostR}><button>New Post</form>    <form method=get action=@{SettingsR}><button>Settings
-                    <h2>Posts
+                    <button>Login    <form method=get action=@{AddPostR}><button>New Post</form>    <form method=get action=@{SettingsR}><button>Settings</form>
                     $if null posts
+                      <h2>Posts
                       No Posts! :(
                     $else
+                      <h2>Posts #{firstPost} - #{lastPost}
                       $forall Entity pid (BlogPost title text date) <- posts
                         <div>
                           <article class=post>
@@ -19,6 +25,14 @@ getBlogR site = do
                             #{text}
                           <article class=comment>
                             Hier könnte dein Kommentar stehen!
-                            
+                    <hr>
+                    <table>
+                      <tr>
+                        <td>
+                          <form method=get action=@{BlogR previousPage}><button>Last</button></form>
+                        <td>
+                          Page #{site}
+                        <td>
+                          <form method=get action=@{BlogR nextPage}><button>Next</button></form>
                     <hr>
                   |]
